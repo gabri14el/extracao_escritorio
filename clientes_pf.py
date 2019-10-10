f3aa9da86b1a2ef845518b084879231311a23b45
Gabriel <gabri14el@gmail.com>
2019-10-10 10:34:15 -0300

    pessoas juridicas

--- clientes_pf.py
+++ clientes_pf.py
@@ -12,7 +12,7 @@ from striprtf.striprtf import rtf_to_text
 
 
 con = fdb.connect(
-    host='localhost', database='/home/gabriel/Downloads/_BD/db.FDB',
+    host='localhost', database='/home/gabriel/Downloads/_BD/db1.FDB',
     user='sysdba', password='masterkey', charset='WIN1252'
   )
 
@@ -20,7 +20,7 @@ pf_path = '/home/gabriel/Downloads/Modelo de Migração de Dados (Clientes - PF)
 pf = pd.read_excel(pf_path)
 
 cursor = con.cursor()
-cursor.execute('select CON_NOME,CON_PROFISSAO,CON_CEP,CON_ESTADO, (CON_ENDERECO||\', \'||CON_NUMERO),CON_CIDADE, CON_CPF,CON_IDENTIDADE,CON_PIS,CON_DATANASCIMENTO, CON_TELRESIDENCIAL, CON_TELCOMERCIAL, CON_TELCELULAR, CON_FAX, CON_NACIONALIDADE, CON_ESTADOCIVIL, CON_BAIRRO, CON_EMAIL, CAST(SUBSTRING(CON_OBSERVACOES FROM 1 FOR 32000) AS VARCHAR(32000)) from ADV_CONTATO where CON_TIPO = \'F\'')
+cursor.execute('select CON_NOME,CON_PROFISSAO,CON_CEP,CON_ESTADO, (CON_ENDERECO||\', \'||CON_NUMERO),CON_CIDADE, CON_CPF,CON_IDENTIDADE,CON_PIS,CON_DATANASCIMENTO, CON_TELRESIDENCIAL, CON_TELCOMERCIAL, CON_TELCELULAR, CON_FAX, CON_NACIONALIDADE, CON_ESTADOCIVIL, CON_BAIRRO, CON_EMAIL, CON_HOMEPAGE, CAST(SUBSTRING(CON_OBSERVACOES FROM 1 FOR 32000) AS VARCHAR(32000)) from ADV_CONTATO where CON_TIPO = \'F\'')
 
 data = cursor.fetchall()
 
@@ -35,7 +35,7 @@ for c in data:
     c.insert(22, None)
     c.insert(22, None)
     c.insert(22, None)
-    c.insert(22, None)
+    #campo livrw 2 corresponde ao item "home page" do banco de dado 
     if c[-1] != None: 
 #        c[-1] =c[-1].decode('windows-1252')
         c[-1] = rtf_to_text(c[-1])
@@ -45,5 +45,5 @@ for c in data:
     pf = pf.append(s, ignore_index=True)
 
 pf.fillna(value=pd.np.nan, inplace=True)
-pf.to_excel('/home/gabriel/clientes.xlsx', engine='xlsxwriter')
+pf.to_excel('/home/gabriel/clientes_pf.xlsx', engine='xlsxwriter', index=False)
 
---
+++ clientes_pj.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import fdb
+import numpy as np 
+from striprtf.striprtf import rtf_to_text
+
+
+con = fdb.connect(
+    host='localhost', database='/home/gabriel/Downloads/_BD/db1.FDB',
+    user='sysdba', password='masterkey', charset='WIN1252'
+  )
+
+pf_path = '/home/gabriel/Downloads/Modelo de Migração de Dados (Clientes - PJ).xls'
+pf = pd.read_excel(pf_path)
+
+sql= ''' 
+SELECT r.CON_NOME, r.CON_JURIDICA_NOMEFANTASIA, r.CON_CGC, r.CON_JURIDICA_CONTATO1, r.CON_JURIDICA_RAMOATIVIDADE, 
+r.CON_INSCESTADUAL, r.CON_INSCMUNICIPAL, r.CON_JURIDICA_TELCONTATO1, r.CON_JURIDICA_TELCONTATO2, r.CON_EMAIL, r.CON_FAX,
+r.CON_HOMEPAGE, r.CON_CIDADE, r.CON_ESTADO, r.CON_CEP, r.CON_BAIRRO, r.CON_ENDERECO, 
+r.CON_JURIDICA_CONTATO2, r.CON_TELCOMERCIAL, CAST(SUBSTRING(CON_OBSERVACOES FROM 1 FOR 32000) AS VARCHAR(32000))
+FROM ADV_CONTATO r where r.CON_TIPO = 'J'
+'''
+cursor = con.cursor()
+cursor.execute(sql)
+
+data = cursor.fetchall()
+
+for c in data:
+    c = list(c)
+    
+    c.insert(0, None)
+    c.insert(0, None)
+    
+    c.insert(12, None)
+    if c[-1] != None: 
+#        c[-1] =c[-1].decode('windows-1252')
+        c[-1] = rtf_to_text(c[-1])
+    else:
+        c[-1] = ''
+    s = pd.Series(c, index= pf.columns)
+    pf = pf.append(s, ignore_index=True)
+
+pf.fillna(value=pd.np.nan, inplace=True)
+pf.to_excel('/home/gabriel/clientes_pj.xlsx', engine='xlsxwriter', index=False)
+
